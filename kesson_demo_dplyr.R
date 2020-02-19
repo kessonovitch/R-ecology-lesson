bb8addbf905d8ad26efac035950eee691d2c9176
mlw406 <k.magid@qmul.ac.uk>
2020-02-19 13:15:43 +0000

    added further script for 17FEB workshop

--- kesson_demo_dplyr.R
+++ kesson_demo_dplyr.R
@@ -11,7 +11,7 @@ surveys <- read_csv("https://ndownloader.figshare.com/files/2292169")
 surveys
 str(surveys)
 # view(surveys)
-#nnote how I put a # in front of the line of code view(surveys) because I don't necessarily want to run it every time I run this script. Some people will also include a note saying #NOT RUN before code they leave in place but don't need every time.
+#note how I put a # in front of the line of code view(surveys) because I don't necessarily want to run it every time I run this script. Some people will also include a note saying #NOT RUN before code they leave in place but don't need every time.
 
 # select data
 select(surveys, c(plot_id, species_id, weight))
@@ -49,6 +49,83 @@ surveys <- surveys %>%
 
 # an example of how we might group data, then summarise it.
 surveys %>%
-  group_by(sex, species_id) %>%
+  group_by(species_id, sex) %>%
   summarise(mean_weight = mean(weight, na.rm = TRUE))
 
+surveys %>%
+  filter(!is.na(weight)) %>% 
+  group_by(species_id, sex) %>%
+  summarise(mean_weight = mean(weight))
+
+#show more rows of our data
+surveys %>%
+  filter(!is.na(weight)) %>% 
+  group_by(species_id, sex) %>%
+  summarise(mean_weight = mean(weight)) %>% 
+  print(n=25)
+
+#omit missing data for sex
+surveys %>%
+  filter(!is.na(weight)) %>% 
+  filter(!is.na(sex)) %>% 
+  group_by(species_id, sex) %>%
+  summarise(mean_weight = mean(weight)) %>% 
+  print(n=25)
+
+#create minimum weight variable
+surveys %>%
+  filter(!is.na(weight)) %>% 
+  filter(!is.na(sex)) %>% 
+  group_by(species_id, sex) %>%
+  summarise(mean_weight = mean(weight),
+            min_weight = min(weight)) %>% 
+  print(n=25)
+
+#arrange groups by minimum weight
+surveys %>%
+  filter(!is.na(weight)) %>% 
+  filter(!is.na(sex)) %>% 
+  group_by(species_id, sex) %>%
+  summarise(mean_weight = mean(weight),
+            min_weight = min(weight)) %>% 
+  arrange(min_weight) %>% 
+  print(n=25)
+
+#make new object from table
+sex_by_sp <- surveys %>%
+  filter(!is.na(weight)) %>% 
+  filter(!is.na(sex)) %>% 
+  group_by(species_id, sex) %>%
+  summarise(mean_weight = mean(weight),
+            min_weight = min(weight)) %>% 
+  arrange(min_weight)
+
+sex_by_sp %>%
+  select(species_id:min_weight)
+
+#arrange by decreasing minimum weight
+surveys %>%
+  filter(!is.na(weight)) %>% 
+  filter(!is.na(sex)) %>% 
+  group_by(species_id, sex) %>%
+  summarise(mean_weight = mean(weight),
+            min_weight = min(weight)) %>% 
+  arrange(desc(min_weight)) %>% 
+  print(n=25)
+
+#count number of cases by sex
+surveys %>% 
+  count(sex)
+
+#same output
+surveys %>%
+  group_by(sex) %>%
+  summarise(count = n())
+
+#sort descending number
+surveys %>% 
+  count(sex, sort = T)
+
+#count by sex and species
+surveys %>% 
+  count(sex, species)
